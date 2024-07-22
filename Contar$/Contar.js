f370ce81104bc1c9b5ca33448415f49646957c54
gimenezhiago <163415252+gimenezhiago@users.noreply.github.com>
2024-07-22 13:49:26 -0300

    kjhggv

--- Contar$/Contar.js
+++ Contar$/Contar.js
@@ -1,21 +1,21 @@
 var btn = document.querySelector("#button");
-var res = document.querySelector("#res");
 
 btn.addEventListener("click", Contar);
 
 function Contar() {
+  var res = document.querySelector("#res");
+
   var start = parseFloat(document.querySelector("input#start").value);
   var end = parseFloat(document.querySelector("#end").value);
   var pass = parseFloat(document.querySelector("#pass").value);
 
-  if (start > end || start == "" || end == "" || pass == "") {
-    alert("Os dados estão errados [ERRO]");
+  if (start.length == 0 || end.length == 0 || pass.length == 0) {
+    window.alert("Os dados estão errados [ERRO]");
   } else {
-
-    res.innerHTML = ''
+    res.innerHTML = `contando`;
 
     for (var r = start; r <= end; r += pass) {
-      res.innerHTML += `${r}`;
+      res.innerHTML += `oii ${r}`;
     }
   }
 }
