714b97d500b2660176facf4bc906f2f9d9496cc5
gimenezhiago <163415252+gimenezhiago@users.noreply.github.com>
2024-08-04 20:30:00 -0300

    jgf

---
+++ Caracter/Caracter.js
@@ -0,0 +1,17 @@
+var area = document.querySelector("#area")
+var select = document.querySelector("#select")
+var res = document.querySelector("#res")
+
+function letter() {
+    let answer
+    area.lenght() = answer
+    res.innerHTML = ""
+    res.innerHTML = `O texto tem ${answer}` 
+}
+
+function text() {
+    let answer
+    area.contains() = answer
+    res.innerHTML = ""
+    res.innerHTML = `O texto tem ${answer}` 
+}
