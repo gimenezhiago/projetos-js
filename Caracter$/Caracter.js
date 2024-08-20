7a00071d61b864e719ebe013f3c37e2daf89e47b
gimenezhiago <163415252+gimenezhiago@users.noreply.github.com>
2024-08-20 08:40:52 -0300

    sfg

--- Caracter$/Caracter.js
+++ Caracter$/Caracter.js
@@ -2,25 +2,20 @@ function btn() {
   var area = document.querySelector("#write").value;
   var select = document.querySelector("#select").value;
   var res = document.querySelector("#res");
-  
+  var answer = "";
+
   res.innerHTML = "";
   if (area == "") {
-    window.alert("Dados em branco")
+    window.alert("Dados em branco");
   } else {
     switch (select) {
       case "letter":
-        for (let i = 0; i <= area.length; i++) {
-          var answer = '';
-            answer = i
-        }
+        answer = area.replace("","").length
         res.innerHTML = answer;
         break;
-        case "text":
-          let arrayAnswer = [];
-          for (let i = 0; i < area.length; i++) {
-            arrayAnswer.push(`Text ${i + 1}: ${area[i]}`);
-          }
-        res.innerHTML = answer
+      case "text":
+        answer = area.split("").length;
+        res.innerHTML = answer;
         break;
     }
   }
